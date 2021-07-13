1d25f8234ff68544631903e2b1799141ddfa2af5
dev-Lesser <dev.jongmoon@gmail.com>
2021-07-13 16:03:15 +0900

    :art: 수집 코드&인덱스 부분 추가 :bug: NULL next page token in while loop

--- index.js
+++ index.js
@@ -1,72 +1,48 @@
-// root: jsname=fk8dgd
-
-// 사용자이름 span class X43Kjb
-// 별점 div class pf5lIe > div aria-label= 5점만점에 3점을 맞았습니다
-// 날짜 span class=p2TkOb
-// 추천수 div class="XlMhZe" > div class="jUL89d y92BAb K3ZHGe" text
-// 리뷰 span jsname="bN97Pc" text
-
 var gplay = require('google-play-scraper');
- 
-// This example will return 3000 reviews
-// on a single call
-// gplay.reviews({
-//   appId: 'droom.sleepIfUCan',
-//   sort: gplay.sort.NEWEST,
-//   num: 3000,
-//   lang: 'ko',
-//   country: 'kr',
-// }).then(console.log, console.log);
- 
 var MongoClient = require('mongodb').MongoClient;
+
 var url = "mongodb://localhost:27017/";
+const DB_NAME = "droom"
+const COLLECTION_NAME = "reviews"
+let nextToken = null
 
-
-// function()
 const getReviews = async () => {
+    var token = nextToken;
     var reviews = await gplay.reviews({
         appId: 'droom.sleepIfUCan',
-        sort: gplay.sort.HELPFULNESS,
-        paginate: true,
+        sort: gplay.sort.NEWEST,
+        paginate: false,
         lang: 'ko',
         country: 'kr',
-        num:1,
-        nextPaginationToken: null // you can omit this parameter
-      })//.then(console.log)  // -> {data:Array, nextPaginationToken: String}
-    //   console.log(reviews)
-      return reviews
+        num:1400,
+        nextPaginationToken: token 
+    }) // -> {data:Array, nextPaginationToken: String}
+    return reviews
 }
-MongoClient.connect(url, {useUnifiedTopology:true}, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("droom");
-    getReviews()
-    .then(res =>{
-        // console.log(res.data)
-        // console.log(res.nextPaginationToken)
-        
-            
-            for (var review in res){
-                var myobj = review;
-                console.log(review)
-                dbo.collection("reviews").insertOne(myobj, function(err, res) {
-                    if (err) throw err;
-                    console.log("1 document inserted");
-                    // db.close();
-                  });
-            }
-            
-            
-          });
-    console.log(12)
-        //   db.close();
-    });
-// console.log(res.data)
 
-// gplay.reviews({
-//   appId: 'droom.sleepIfUCan',
-//   sort: gplay.sort.NEWEST,
-//   paginate: true,
-//   lang: 'ko',
-//   country: 'kr',
-//   nextPaginationToken: 'TOKEN_FROM_THE_PREVIOUS_REQUEST' // you can omit this parameter
-// }).then(console.log, console.log);
+MongoClient.connect(url, {useUnifiedTopology:true}, async function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(DB_NAME);
+    await dbo.collection(COLLECTION_NAME).createIndex({ date: -1 }).then(res=>{console.log(`Index created: ${res}`);})
+    await dbo.collection(COLLECTION_NAME).createIndex({ score: -1 }).then(res=>{console.log(`Index created: ${res}`);})
+    await dbo.collection(COLLECTION_NAME).createIndex({ replyDate: -1 }).then(res=>{console.log(`Index created: ${res}`);})
+    await dbo.collection(COLLECTION_NAME).createIndex({ version: -1 }).then(res=>{console.log(`Index created: ${res}`);})
+    await dbo.collection(COLLECTION_NAME).createIndex({ userName: -1 }).then(res=>{console.log(`Index created: ${res}`);})
+    
+
+    var loop =0
+    while (true){
+        try{
+            var res = await getReviews()
+            dbo.collection(COLLECTION_NAME).insertMany(res.data)
+            nextToken = res.nextPaginationToken;
+            loop = loop + res.data.length;
+            console.log(nextToken, loop)
+            if (nextToken == null) {
+                break
+            }
+        } catch(err){
+            
+        }
+    }
+});
